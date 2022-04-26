5b4360babf9292aee677eaf45973c933349015da
Jayakumar <jayakumar2242@gmail.com>
2022-04-26 13:41:38 +0530

    New project into git

---
+++ src/org/emp/Employe.java
@@ -0,0 +1,35 @@
+package org.emp;
+
+public class Employe {
+	public void empid() {
+		System.out.println("345");
+	}
+	public void empname() {
+		System.out.println("jaikumer");
+	}
+	public void empdop() {
+		System.out.println("10/07/1998");
+	}
+	public void empphone() {
+		System.out.println("7358516218");	
+	}
+	public void empemail() {
+		System.out.println("jayakumar2242@gmail.com");
+	
+	}
+	public void empaddress() {
+		System.out.println("chennai");
+		
+	}
+	public static void main(String[] args) {
+		Employe E = new Employe();
+		E.empid();
+		E.empname();
+		E.empdop();
+		E.empphone();
+		E.empemail();
+		E.empaddress();
+				
+	}
+
+}
